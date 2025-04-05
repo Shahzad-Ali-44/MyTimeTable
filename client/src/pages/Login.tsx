81f3dcc264c1e4bed7be0afaec8d9181c855bec9
Shahzad Ali <shahzad.ali41410@gmail.com>
2025-04-05 20:35:01 +0500

    fix footer layout

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -102,7 +102,7 @@ export function LoginForm({
 
 export default function Page() {
   return (
-    <div className="flex md:min-h-screen w-full items-center justify-center p-6 md:p-10">
+    <div className="flex min-h-screen w-full items-center justify-center p-6 md:p-10">
       <div className="w-full max-w-sm">
         <LoginForm />
       </div>
