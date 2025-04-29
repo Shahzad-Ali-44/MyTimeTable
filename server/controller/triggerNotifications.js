6990d910b02e4d1fb1ac849b6f9a31a6f4aef3f4
Shahzad Ali <shahzad.ali41410@gmail.com>
2025-04-29 17:23:12 +0500

    add notification toast

--- server/controller/triggerNotifications.js
+++ server/controller/triggerNotifications.js
@@ -2,24 +2,20 @@ import Task from "../model/taskModel.js";
 import User from "../model/userModel.js";
 import sendNotification from "../firebase.js";
 import moment from "moment-timezone";
-
 export const triggerNotifications = async (req, res) => {
   try {
     const allTasks = await Task.find({ notified: false });
     for (const task of allTasks) {
       const user = await User.findById(task.userId);
       if (!user) continue;
-
       const userTimezone = user.timezone || "Asia/Karachi";
       const nowInUserTimezone = moment().tz(userTimezone);
       const hours = nowInUserTimezone.hours();
       const minutes = nowInUserTimezone.minutes();
       const [time, mod] = task.taskTime.split(" ");
       let [h, m] = time.split(":").map(Number);
-
       if (mod === "PM" && h < 12) h += 12;
       if (mod === "AM" && h === 12) h = 0;
-
       if (h === hours && m === minutes) {
         if (user.firebaseToken) {
           await sendNotification(
@@ -33,7 +29,6 @@ export const triggerNotifications = async (req, res) => {
       }
     }
     res.json({ message: "Notifications API" });
-
   } catch (err) {
     console.error("Error in triggerNotifications:", err);
     res
